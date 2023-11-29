2e760ab11f0c233549dbf592807078ac3082f404
aether-kunn <97568224+Li-is-cool@users.noreply.github.com>
2023-11-28 19:35:44 -0500

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,9 @@
+function addUser() {
+    player1_name = document.getElementById("player1_name_input").value;
+    
+      localStorage.setItem("player1_name", player1_name);
+      
+  
+      window.location = "game_page.html";
+  }
+  
